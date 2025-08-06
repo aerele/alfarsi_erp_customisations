8c743070e26ef53dc811f30304290313ad70f769
Dhinesh <dhinesh@aerele.in>
2025-08-06 19:04:13 +0530

    feat: add for intercompany_stock_transfer

--- alfarsi_erp_customisations/alfarsi_erp_customisations/doctype/intercompany_stock_transfer/intercompany_stock_transfer.py
+++ alfarsi_erp_customisations/alfarsi_erp_customisations/doctype/intercompany_stock_transfer/intercompany_stock_transfer.py
@@ -1,9 +1,119 @@
 # Copyright (c) 2025, Alfarsi and contributors
 # For license information, please see license.txt
 
-# import frappe
+import json
+from datetime import date
+
+import frappe
 from frappe.model.document import Document
 
 
 class IntercompanyStockTransfer(Document):
     pass
+
+
+@frappe.whitelist()
+def get_stock_in_other_companies(item_list, current_company):
+    item_list = json.loads(item_list)
+    result = []
+    for item_code in item_list:
+        item = item_code["item_code"]
+        result += (
+            frappe.db.sql(
+                f"""select
+															bin.item_code,
+															i.item_name,
+															bin.actual_qty,
+															bin.warehouse,
+															c.name as company
+															from `tabBin` as bin
+															join `tabWarehouse` as w on bin.warehouse = w.name
+															join `tabCompany` as c on c.name = w.company
+															join `tabItem` as i on i.name = bin.item_code
+															where c.name != '{current_company}'
+															And
+															bin.item_code = '{item}'
+															And
+															bin.actual_qty > 0
+															""",
+                as_dict=1,
+            )
+            or []
+        )
+    if result:
+        return result
+    frappe.msgprint("No Item Found  in another Company")
+    return None
+
+
+@frappe.whitelist()
+def creat_intercompany_stock_transfer(transfer_details):
+    is_enable = frappe.db.get_singles_value("Intercompany Stock Transfer", "enable")
+    if is_enable:
+        transfer_details = json.loads(transfer_details)
+        posting_date = date.today()
+        posting_date = posting_date.strftime("%Y-%m-%d")
+        for transfer_detail in transfer_details:
+            material_issue_doc = frappe.new_doc("Stock Entry")
+            material_issue_doc.stock_entry_type = "Material Issue"
+            material_issue_doc.company = frappe.db.get_value(
+                "Warehouse", transfer_detail.get("s_warehouse"), "company"
+            )
+            material_issue_doc.posting_date = posting_date
+            batch_no = transfer_detail.get("batch")
+            supplier_batch_no = expiry_date = None
+
+            if batch_no:
+                supplier_batch_no, expiry_date = frappe.db.get_value(
+                    "Batch", batch_no, ("supplier_batch_no", "expiry_date")
+                )
+            material_issue_doc.append(
+                "items",
+                {
+                    "s_warehouse": transfer_detail.get("s_warehouse"),
+                    "item_code": transfer_detail.get("item_code"),
+                    "qty": transfer_detail.get("qty"),
+                    "batch_no": batch_no,
+                    "supplier_batch_no": supplier_batch_no,
+                    "expiry_date": expiry_date,
+                    "expense_account": frappe.db.get_value(
+                        "Intercompany Stock Transfer Table",
+                        {
+                            "company": transfer_detail.get("out_company"),
+                            "parent": "Intercompany Stock Transfer",
+                        },
+                        "stock_in_account_booking",
+                    ),
+                },
+            )
+            material_issue_doc.save(ignore_permissions=True)
+            material_issue_doc.submit()
+            material_receipt_doc = frappe.new_doc("Stock Entry")
+            material_receipt_doc.stock_entry_type = "Material Receipt"
+            material_receipt_doc.company = frappe.db.get_value(
+                "Warehouse", transfer_detail.get("t_warehouse"), "company"
+            )
+            material_receipt_doc.posting_date = posting_date
+
+            material_receipt_doc.append(
+                "items",
+                {
+                    "t_warehouse": transfer_detail.get("t_warehouse"),
+                    "item_code": transfer_detail.get("item_code"),
+                    "qty": transfer_detail.get("qty"),
+                    "batch_no": transfer_detail.get("batch_no"),
+                    "expiry_date": expiry_date,
+                    "supplier_batch_no": supplier_batch_no,
+                    "expense_account": frappe.db.get_value(
+                        "Intercompany Stock Transfer Table",
+                        {
+                            "company": transfer_detail.get("in_company"),
+                            "parent": "Intercompany Stock Transfer",
+                        },
+                        "stock_in_account_booking",
+                    ),
+                },
+            )
+
+            material_receipt_doc.save(ignore_permissions=True)
+            material_receipt_doc.submit()
--- alfarsi_erp_customisations/public/js/delivery_note.js
+++ alfarsi_erp_customisations/public/js/delivery_note.js
@@ -1,21 +1,170 @@
 frappe.ui.form.on("Delivery Note", {
-	refresh:function(frm){
-		if(
-			!frm.__islocal
-			&& frappe.model.can_create("Packing Slip")
-			&& frm.doc.docstatus != 2
-		) {
+	refresh: function (frm) {
+		if (!frm.__islocal && frappe.model.can_create("Packing Slip") && frm.doc.docstatus != 2) {
 			frm.add_custom_button(
-				__('Packing Slip'),
-				function() {
+				__("Packing Slip"),
+				function () {
 					frappe.model.open_mapped_doc({
 						method: "alfarsi_erp_customisations.overrides.delivery_note.custom_make_packing_slip",
 						frm: me.frm
 					});
 				},
-				__('Create')
+				__("Create")
 			);
 		}
-	}
-})
+	},
+	custom_see_stock_in_other_companies: function (frm) {
+		frappe.call({
+			method: "alfarsi_erp_customisations.alfarsi_erp_customisations.doctype.intercompany_stock_transfer.intercompany_stock_transfer.get_stock_in_other_companies",
+			args: { item_list: frm.doc.items, current_company: frm.doc.company },
+			freeze: true,
+			freeze_message: __("Fetching Other Companies Stock"),
+			callback: (r) => {
+				if (r.message) {
+					const fields = [
+						{
+							fieldtype: "Link",
+							fieldname: "item_code",
+							read_only: 1,
+							options: "Item",
+							in_list_view: 1,
+							label: "Item Code",
+							columns: 1,
+						},
+						{
+							fieldtype: "Data",
+							fieldname: "item_name",
+							read_only: 1,
+							in_list_view: 1,
+							label: "Item Name",
+							columns: 5,
+						},
+						{
+							fieldtype: "Data",
+							fieldname: "actual_qty",
+							read_only: 1,
+							in_list_view: 1,
+							columns: 1,
+							label: "Qctual Qty",
+						},
+						{
+							fieldtype: "Link",
+							fieldname: "warehouse",
+							read_only: 1,
+							in_list_view: 1,
+							options: "warehouse",
+							label: "Warehouse",
+						},
+						{
+							fieldtype: "Link",
+							fieldname: "company",
+							read_only: 1,
+							in_list_view: 1,
+							options: "Company",
+							label: "Company",
+						},
+					];
+					var data = r.message.map((d) => {
+						return {
+							item_code: d.item_code,
+							actual_qty: d.actual_qty,
+							warehouse: d.warehouse,
+							company: d.company,
+							item_name: d.item_name,
+						};
+					});
 
+					let dialog = new frappe.ui.Dialog({
+						title: __("Stock In Other Companies"),
+						size: "extra-large",
+						fields: [
+							{
+								read_only: 1,
+								fieldname: "stock_in_other_companies",
+								fieldtype: "Table",
+								label: "Items",
+								cannot_add_rows: true,
+								cannot_delete_rows: true,
+								data: data,
+								get_data: () => {
+									return data;
+								},
+								fields: fields,
+							},
+						],
+					});
+
+					dialog.show();
+				}
+			},
+		});
+	},
+	custom_intercompany_stock_transfer: function (frm) {
+		const fields = [
+			{
+				fieldtype: "Link",
+				fieldname: "s_warehouse",
+				options: "Warehouse",
+				in_list_view: 1,
+				label: "From Warehouse",
+				reqd: 1,
+			},
+
+			{
+				fieldtype: "Link",
+				fieldname: "t_warehouse",
+				options: "Warehouse",
+				in_list_view: 1,
+				label: "To Warehouse",
+				reqd: 1,
+			},
+			{
+				fieldtype: "Link",
+				fieldname: "item_code",
+				options: "Item",
+				in_list_view: 1,
+				label: "Item Code",
+				reqd: 1,
+			},
+			{
+				fieldtype: "Float",
+				fieldname: "qty",
+				default: 0,
+				read_only: 0,
+				in_list_view: 1,
+				label: __("Qty"),
+			},
+			{
+				fieldtype: "Link",
+				fieldname: "batch",
+				options: "Batch",
+				in_list_view: 1,
+				label: "Batch",
+			},
+		];
+		let dialog = new frappe.ui.Dialog({
+			title: __("Intercompany Stock Transfer"),
+			size: "extra-large",
+			fields: [
+				{
+					fieldname: "intercompany_stock_transfer",
+					fieldtype: "Table",
+					label: "Items",
+					fields: fields,
+				},
+			],
+			primary_action: function () {
+				var data = dialog.get_value("intercompany_stock_transfer");
+				frappe.call({
+					method: "alfarsi_erp_customisations.alfarsi_erp_customisations.doctype.intercompany_stock_transfer.intercompany_stock_transfer.creat_intercompany_stock_transfer",
+					args: {
+						transfer_details: data,
+					},
+					callback: function (r) {},
+				});
+			},
+			primary_action_label: __("Stock Transfer"),
+		});
+		dialog.show();
+	},
+});
