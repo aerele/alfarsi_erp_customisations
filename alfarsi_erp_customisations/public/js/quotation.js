4545d84b4b678f7d69bab0883674c2c3c86f3f2f
Afsalsyed <afsalsyed12@gmail.com>
2025-08-19 14:33:01 +0530

    feat : for quotation from ecomm, get last purchase rate for user if the user has already completed a quotation

--- alfarsi_erp_customisations/public/js/quotation.js
+++ alfarsi_erp_customisations/public/js/quotation.js
@@ -1,4 +1,22 @@
 frappe.ui.form.on('Quotation', {
+    refresh: function (frm) {
+        if(frm.doc.docstatus == 0 && frm.doc.medusa_quotation_id) {
+		frm.add_custom_button(__('Get Last Purchase Rate'), function(){
+			frappe.call({
+				method: "alfarsi_erp_customisations.public.py.last_purchase_rate_in_quotation.get_last_purchase_rates",
+				args: {
+					items: frm.doc.items.map(d => d.item_code),
+					customer: frm.doc.party_name
+				},
+				callback: function(r) {
+					if(r.message) {
+						show_last_rates_modal(r.message, frm);
+					}
+				}
+			});
+		});
+	}
+    },
     custom_ministry_quotation: function (frm) {
         let item_settings
         if (frm.doc.custom_ministry_quotation) {
@@ -52,4 +70,101 @@ function calculate_total_price(frm, cdt, cdn) {
     let row = locals[cdt][cdn];
     row.custom_t_price_ro = (row.qty || 0) * (row.custom_u_price_ro || 0);
     frm.refresh_field("items");
+}
+
+
+
+function show_last_rates_modal(data, frm) {
+  let dialog = new frappe.ui.Dialog({
+    title: __("Last Approved Rates"),
+    fields: [
+      {
+        fieldname: "rates_html",
+        fieldtype: "HTML",
+      }
+    ],
+    primary_action_label: __("Use Selected Prices"),
+    primary_action() {
+      let selected = [];
+      dialog.fields_dict.rates_html.$wrapper.find(".use-price-checkbox:checked").each(function() {
+        let idx = $(this).data("idx");
+        selected.push(data[idx]);
+      });
+
+      if (!selected.length) {
+        frappe.msgprint(__("Please select at least one item."));
+        return;
+      }
+
+      selected.forEach(row => {
+        if(row.last_rate) {
+          let item_row = frm.doc.items.find(i => i.item_code === row.item_code);
+          if(item_row) {
+            frappe.model.set_value(item_row.doctype, item_row.name, "rate", row.last_rate);
+            frappe.model.set_value(item_row.doctype, item_row.name, "standard_price", row.last_rate);
+						frappe.show_alert({ message: __("Selected rates applied successfully"), indicator: "green" });
+          }
+				} else {
+					frappe.msgprint(__("No last rate found for item {0}", [row.item_name]));
+        }
+      });
+
+      dialog.hide();
+    }
+  });
+
+  let html = `<table class="table table-bordered">
+                <thead>
+                  <tr>
+                    <th><input type="checkbox" id="select-all"></th>
+                    <th>Item Code</th>
+                    <th>Item Name</th>
+                    <th>Last Rate</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>`;
+  
+  data.forEach((d, idx) => {
+    let formatted_rate = d.last_rate !== null 
+      ? format_currency(d.last_rate, frappe.defaults.get_default("currency"), 3) 
+      : "-";
+
+    html += `<tr>
+               <td><input type="checkbox" class="use-price-checkbox" data-idx="${idx}"></td>
+               <td>${d.item_code}</td>
+               <td>${d.item_name}</td>
+               <td>${formatted_rate}</td>
+               <td><button class="btn btn-xs btn-primary use-price-btn" data-idx="${idx}">Use Price</button></td>
+             </tr>`;
+  });
+
+  html += "</tbody></table>";
+  
+  dialog.fields_dict.rates_html.$wrapper.html(html);
+
+  dialog.fields_dict.rates_html.$wrapper.find(".use-price-btn").on("click", function() {
+    let idx = $(this).data("idx");
+    let row = data[idx];
+
+    if(row.last_rate) {
+      let item_row = frm.doc.items.find(i => i.item_code === row.item_code);
+      if(item_row) {
+        frappe.model.set_value(item_row.doctype, item_row.name, "rate", row.last_rate);
+        frappe.model.set_value(item_row.doctype, item_row.name, "standard_price", row.last_rate);
+        frappe.show_alert({ message: __("Rate updated for {0}", [row.item_code]), indicator: "green" });
+      } else {
+        frappe.msgprint(__("Item {0} not found in current Quotation", [row.item_code]));
+      }
+    } else {
+      frappe.msgprint(__("No last rate found for {0}", [row.item_code]));
+    }
+  });
+
+  dialog.fields_dict.rates_html.$wrapper.find("#select-all").on("change", function() {
+    let checked = $(this).prop("checked");
+    dialog.fields_dict.rates_html.$wrapper.find(".use-price-checkbox").prop("checked", checked);
+  });
+
+  dialog.show();
 }
