945fd7c2ef351d6569e9ffd30904e53756c1a387
Pandiyan P <pandiyanpalani37@gmail.com>
2025-11-05 18:48:00 +0530

    Adding Validation and creating documents in Lexer

---
+++ alfarsi_erp_customisations/alfarsi_erp_customisations/doctype/lexer_import_log/lexer_import_log.js
@@ -0,0 +1,23 @@
+frappe.ui.form.on('Lexer Import Log', {
+    refresh: function(frm) {
+        frm.add_custom_button('Validate Items', () => {
+            frappe.call({
+                method: "alfarsi_erp_customisations.alfarsi_erp_customisations.doctype.lexer_import_log.lexer_import_log.validate_items",
+                args: { docname: frm.doc.name },
+                callback() {
+                    frappe.msgprint("Item codes validated & updated successfully.");
+                    frm.reload_doc();
+                }
+            });
+        });
+
+        frm.add_custom_button('Create Document', function() {
+            frappe.call({
+                method: 'alfarsi_erp_customisations.alfarsi_erp_customisations.doctype.lexer_import_log.lexer_import_log.create_documents',
+                args: {
+                     docname: frm.doc.name
+                   },
+            });
+        });
+    },
+});
