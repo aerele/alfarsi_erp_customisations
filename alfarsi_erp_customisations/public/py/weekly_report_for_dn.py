f15d7e48b3a1a445f55cd476be0aaefb9c119a13
Pandiyan P <pandiyanpalani37@gmail.com>
2025-11-19 13:36:46 +0530

    The Report Sending file Has to be Mergerd In Single File

--- alfarsi_erp_customisations/public/py/weekly_report_for_dn.py
+++ alfarsi_erp_customisations/public/py/weekly_report_for_dn.py
@@ -47,5 +47,54 @@ def get_weekly_report_for_dn():
         }]
     )
 
+    owner_map={}
+    for row in data:
+        owner = row.get("owner")
+        dn_name = row.get("delivery_note")
+        if owner not in owner_map:
+            owner_map[owner] = []
+        owner_map[owner].append(dn_name)
+
+    for owner, dn_list in owner_map.items():
+            table_rows = ""
+            for dn in dn_list:
+                table_rows += f"""
+                    <tr>
+                        <td style='padding:6px; border:1px solid #ccc;'>{dn}</td>
+                    </tr>
+                """
+
+            html = f"""
+                <h2>Pending Delivery Note Report</h2>
+                <p><b>Date :</b> {from_date} to {to_date}</p>
+
+                <table border='1' cellspacing='0' cellpadding='6'
+                    style='border-collapse: collapse; width: 100%;'>
+                    <thead>
+                        <tr>
+                            <th style='padding:8px; border:1px solid #ccc; background:#f2f2f2;'>
+                                Delivery Note
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {table_rows}
+                    </tbody>
+                </table>
+            """
+            pdf_content = get_pdf(html)
+            frappe.sendmail(
+                recipients=[owner],
+                subject=f"Pending Delivery Note Report ({from_date} to {to_date})",
+                attachments=[{
+                    "fname": f"Pending_DN_Report_{owner}.pdf",
+                    "fcontent": pdf_content
+                }],
+                message = f"""
+                <b>Make sure to fill the pending fields for these Delivery Notes:</b><br>
+                - Receipt Voucher<br>
+                - Signed & Stamped Delivery Note Copy<br><br>
+                """
+            )
     frappe.msgprint("Weekly PDF Report Sent Successfully.")
     return "Weekly PDF Report Sent Successfully"
