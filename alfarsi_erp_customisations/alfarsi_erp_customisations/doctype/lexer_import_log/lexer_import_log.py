7c70ed6524edd37056fa4a7422caaf90aa7ad0db
Pandiyan P <pandiyanpalani37@gmail.com>
2025-11-25 12:14:30 +0530

    changing the item to link field and made some modifications

--- alfarsi_erp_customisations/alfarsi_erp_customisations/doctype/lexer_import_log/lexer_import_log.py
+++ alfarsi_erp_customisations/alfarsi_erp_customisations/doctype/lexer_import_log/lexer_import_log.py
@@ -13,7 +13,7 @@ class LexerImportLog(Document):
 @frappe.whitelist()
 def validate_items(docname):
     doc = frappe.get_doc("Lexer Import Log", docname)
-
+    
     for row in doc.items:
         part_number = row.part_number
         if not part_number:
@@ -28,6 +28,7 @@ def validate_items(docname):
 
         if item:
             row.item_code = item["name"]
+            row.db_update()
         else:
             new_item_name = f"Trusta {row.item_name or ''} ANE - {part_number}"
             last_code = frappe.db.sql(
@@ -93,15 +94,17 @@ def duplicate_reference_docs_from_settings(doc):
                 {
                     "item_code": item.item_code,
                     "qty": item.qty,
-                    "rate": item.purchase_rate,
+                    "rate": item.purchase_rate,              
                 },
             )
         new_po.transaction_date = getdate(doc.get("purchase_date"))
         new_po.schedule_date = getdate(doc.get("purchase_date"))
         new_po.payment_schedule = []
         new_po.insert()
+        new_po.submit()
 
         supplier_currency = orig_po.get("currency")
+
         pr_items = []
         for item in new_po.items:
             pr_items.append(
@@ -123,7 +126,8 @@ def duplicate_reference_docs_from_settings(doc):
             }
         )
         pr.insert()
-
+        pr.submit()
+  
         pi_items = []
         for item in new_po.items:
             pi_items.append(
@@ -136,14 +140,13 @@ def duplicate_reference_docs_from_settings(doc):
                 }
             )
 
-        cost_center = new_po.get("cost_center")
+        cost_center = new_po.get("cost_center") or frappe.db.get_value("Company", new_po.company, "cost_center")
         if not cost_center:
             frappe.throw("Cost Center is not set in the Purchase Order.")
 
         custom_purchase_invoice_type = (
             new_po.get("custom_purchase_invoice_type") or "Import Purchase - Supplier"
         )
-
         pi = frappe.get_doc(
             {
                 "doctype": "Purchase Invoice",
@@ -163,46 +166,51 @@ def duplicate_reference_docs_from_settings(doc):
             }
         )
         pi.insert()
-
+        
     if reference_sales_order:
         orig_so = frappe.get_doc("Sales Order", reference_sales_order)
         new_so = frappe.copy_doc(orig_so)
         new_so.name = None
         new_so.items = []
+        new_so.order_contains_free_item = 0
+        new_so.workflow_state = "Draft"
         for item in doc.items:
             new_so.append(
                 "items",
-                {"item_code": item.item_code, "qty": item.qty, "rate": item.sales_rate},
+                {
+                    "item_code": item.item_code,
+                    "qty": item.qty,
+                    "rate": item.sales_rate,
+                },
             )
+
         new_so.po_no = doc.get("cust_po_no")
         new_so.transaction_date = getdate(doc.get("sale_date"))
         new_so.delivery_date = getdate(doc.get("sale_date"))
         new_so.payment_schedule = []
-        new_so.insert()
+        new_so.insert(ignore_permissions=True)
+        new_so.submit()
+
 
         dn_items = []
         for item in new_so.items:
             matched_so_item = next(
-                (
-                    so_item
-                    for so_item in new_so.items
-                    if so_item.item_code == item.item_code
-                    and so_item.qty == item.qty
-                    and so_item.rate == item.rate
-                ),
+                (so_item for so_item in new_so.items if so_item.item_code == item.item_code),
                 None,
             )
+            if not matched_so_item:
+                frappe.throw(f"SO Item not found for {item.item_code}")
+
             dn_items.append(
                 {
                     "item_code": item.item_code,
-                    "qty": item.qty or 0,
-                    "rate": item.rate or 0,
+                    "qty": item.qty,  
+                    "rate": item.rate,
                     "sales_order": new_so.name,
-                    "so_detail": matched_so_item.name if matched_so_item else None,
+                    "so_detail": matched_so_item.name,
                     "against_sales_order": new_so.name,
                 }
             )
-        print(doc.get("delivery_date"))
         dn = frappe.get_doc(
             {
                 "doctype": "Delivery Note",
@@ -213,6 +221,7 @@ def duplicate_reference_docs_from_settings(doc):
             }
         )
         dn.insert()
+        dn.submit()
 
         si_items = []
         for item in dn_items:
@@ -225,6 +234,7 @@ def duplicate_reference_docs_from_settings(doc):
                     "delivery_note": dn.name,
                 }
             )
+
         si = frappe.get_doc(
             {
                 "doctype": "Sales Invoice",
