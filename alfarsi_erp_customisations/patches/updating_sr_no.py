7c1779d603c4d5a1f8a96a1d6de235b8bf711e91
Pandiyan P <pandiyanpalani37@gmail.com>
2025-12-05 17:51:44 +0530

    changind sr number to delivery note and sales invoice

--- alfarsi_erp_customisations/patches/updating_sr_no.py
+++ alfarsi_erp_customisations/patches/updating_sr_no.py
@@ -9,3 +9,20 @@ def execute():
         frappe.db.set_value('Quotation Item', item.name,'custom_sr_no', old_value)
     print("SR No updated successfully in Custom SR No field")
     frappe.db.commit()
+
+    dn=frappe.get_all('Delivery Note Item', fields=['name','customer_srno','custom_sr_no'])
+    for d in dn:
+        old_value=d.customer_srno
+
+        frappe.db.set_value('Delivery Note Item', d.name,'custom_sr_no', old_value)
+    print("SR No updated successfully in Custom SR No field of Delivery Note Item")
+    frappe.db.commit()
+
+    si=frappe.get_all('Sales Invoice Item', fields=['name','customer_srno','custom_sr_no'])
+    for s in si:
+        old_value=s.customer_srno
+
+        frappe.db.set_value('Sales Invoice Item', s.name,'custom_sr_no', old_value)
+    print("SR No updated successfully in Custom SR No field of Sales Invoice Item")
+
+    frappe.db.commit()
