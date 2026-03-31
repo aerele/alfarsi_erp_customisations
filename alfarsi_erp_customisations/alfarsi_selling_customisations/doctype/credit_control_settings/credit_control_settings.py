a0ecf204f1792b21a82d77a144c595c229c7716b
Arnesh <arnesh1419@gmail.com>
2026-03-31 23:08:45 +0530

    feat:enhance credit control with override functionality

--- alfarsi_erp_customisations/alfarsi_selling_customisations/doctype/credit_control_settings/credit_control_settings.py
+++ alfarsi_erp_customisations/alfarsi_selling_customisations/doctype/credit_control_settings/credit_control_settings.py
@@ -1,8 +1,22 @@
 # Copyright (c) 2026, Alfarsi and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
+
 class CreditControlSettings(Document):
-	pass
+	def validate(self):
+		customers = [row.customers for row in self.customers]
+		if len(customers) != len(set(customers)):
+			frappe.throw("Duplicate customers are not allowed")
+
+		for row in self.customers:
+			if row.exclude_from_credit_control:
+				row.override_limit = 0
+				row.override_used = 0
+				continue
+			if row.override_limit < 0:
+				frappe.throw(f"Override limit cannot be negative for {row.customers}")
+			if row.override_limit > 10:
+				frappe.throw(f"Override limit cannot exceed 10 for {row.customers}")
